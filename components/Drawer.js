4fb3d8efb92272d55332ec4a0195f4641b05414a
fiskhandlarn <oskar@joelson.org>
2021-04-19 21:29:00 +0200

    Add drawer icons

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import LogScreen from '../screens/LogScreen';
@@ -10,8 +11,30 @@ export default class Drawer extends React.Component {
   render() {
     return (
       <D.Navigator initialRouteName="Home">
-        <D.Screen name="Home" component={HomeScreen}/>
-        <D.Screen name="Log" component={LogScreen} />
+        <D.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            drawerIcon: ({focused, size}) => (
+              <MaterialCommunityIcons
+                name="cards"
+                size={size}
+              />
+            ),
+          }}
+        />
+        <D.Screen
+          name="Log"
+          component={LogScreen}
+          options={{
+            drawerIcon: ({focused, size}) => (
+              <MaterialIcons
+                name="view-list"
+                size={size}
+              />
+            ),
+          }}
+        />
       </D.Navigator>
     );
   }
