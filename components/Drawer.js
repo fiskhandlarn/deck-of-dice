2c9b432cf9d3405d507f5cf46e25feb8bf9ea80b
fiskhandlarn <oskar@joelson.org>
2022-05-22 15:02:16 +0200

    Implement dark mode for drawer, style drawer with primary colors

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,71 +1,76 @@
 import * as React from 'react';
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import AboutScreen from '../screens/AboutScreen';
+import ColorMode from '../shared/ColorMode';
 import HomeScreen from '../screens/HomeScreen';
 import LogScreen from '../screens/LogScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, Text } from 'react-native';
 
 // https://snack.expo.io/3vtKkikb7
 const D = createDrawerNavigator();
 
 export default class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+
+    ColorMode.addListener((value) => this.onColorModeChange(value));
+
+    this.state = {
+      colorMode: ColorMode.value(),
+    };
+  }
+
+  onColorModeChange(value) {
+    this.setState({ colorMode: value });
+  }
+
   render() {
     return (
-      <D.Navigator initialRouteName="Home">
+      <D.Navigator
+        initialRouteName='Home'
+        screenOptions={{drawerStyle: {backgroundColor: StyleSheet.flatten(ColorMode.styles().container).backgroundColor}}}
+      >
         <D.Screen
-          name="Home"
+          name='Home'
           component={HomeScreen}
-          options={{
-            drawerIcon: ({focused, size}) => (
-              <MaterialCommunityIcons
-                name='cards'
-                size={size}
-              />
-            ),
-            headerShown: false,
-          }}
+          options={this.screenOptions('Home', MaterialCommunityIcons, 'cards')}
         />
         <D.Screen
-          name="Log"
+          name='Log'
           component={LogScreen}
-          options={{
-            drawerIcon: ({focused, size}) => (
-              <MaterialIcons
-                name='view-list'
-                size={size}
-              />
-            ),
-            headerShown: false,
-          }}
+          options={this.screenOptions('Log', MaterialIcons, 'view-list')}
         />
         <D.Screen
-          name="Settings"
+          name='Settings'
           component={SettingsScreen}
-          options={{
-            drawerIcon: ({focused, size}) => (
-              <MaterialIcons
-                name='settings'
-                size={size}
-              />
-            ),
-            headerShown: false,
-          }}
+          options={this.screenOptions('Settings', MaterialIcons, 'settings')}
         />
         <D.Screen
-          name="About"
+          name='About'
           component={AboutScreen}
-          options={{
-            drawerIcon: ({focused, size}) => (
-              <MaterialIcons
-                name='info'
-                size={size}
-              />
-            ),
-            headerShown: false,
-          }}
+          options={this.screenOptions('About', MaterialIcons, 'info')}
         />
       </D.Navigator>
     );
+  }
+
+  screenOptions(title, IconClass, iconHandle) {
+    return {
+      drawerActiveTintColor: ColorMode.primaryLightColor(),
+      drawerLabel: ({focused, size}) => (
+        <Text style={focused ? {color: ColorMode.primaryColor()} : ColorMode.styles().text}>{title}</Text>
+      ),
+      drawerIcon: ({focused, size}) => (
+        <IconClass
+          name={iconHandle}
+          size={size}
+          color={StyleSheet.flatten(ColorMode.styles().text).color}
+        />
+      ),
+      headerShown: false,
+    };
   }
 }
